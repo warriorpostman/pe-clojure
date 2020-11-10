3d62bbcb44474bc2c471c54390c18136caeb8024
Rustam Kasad <rustamkasad@gmail.com>
2020-11-09 23:35:47 -0700

    Add lein-auto.

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
 (defproject pe-clojure "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Project Euler solutions using Clojure"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :main ^:skip-aot pe-clojure.core
+  :plugins [[lein-auto "0.1.3"]]
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
